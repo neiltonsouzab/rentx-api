03416bfcfd96f37ef05bcfc06eac29115d78e08c
Neilton de Souza Barbosa <neilton.barbosa@sefin.ro.gov.br>
2021-09-06 12:24:03 -0400

    implemented storage providers

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,8 +2,21 @@ import { container } from 'tsyringe';
 
 import { IMailProvider } from './MailProvider/IMailProvider';
 import { EtheralMailProvider } from './MailProvider/implementations/EtherealMailProvider';
+import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider';
+import { S3StorageProvider } from './StorageProvider/implementations/S3StorageProvider';
+import { IStorageProvider } from './StorageProvider/IStorageProvider';
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
   new EtheralMailProvider(),
+);
+
+const storageProvider = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  storageProvider[process.env.STORAGE_DRIVER],
 );
